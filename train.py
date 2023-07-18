fd569f1719131642f63e593d06aa26e241f906d8
fvamosi <fgvamosi2@gmail.com>
2023-07-18 15:32:03 -0600

    first idea

--- train.py
+++ train.py
@@ -11,10 +11,10 @@ import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
 
-BATCH_SIZE = 128
+BATCH_SIZE = 16
 N_WORKERS = 4
 N_EPOCHS = 15
-MAX_IMAGES = -1
+MAX_IMAGES = 500
 LR = 0.001
 
 def setup(rank, world_size):
@@ -40,7 +40,11 @@ def main(rank, world_size):
 
     # Reading the data and the denormalization function
     # images, mean_age, ages, get_age = read_data("data/91", postfix=".nii.gz", max_entries=MAX_IMAGES)
-    images, mean_age, ages, get_age = read_data("/work/forkert_lab/erik/T1_warped", postfix=".nii.gz", max_entries=MAX_IMAGES)
+    # images, mean_age, ages, get_age = read_data("/work/forkert_lab/erik/T1_warped", postfix=".nii.gz", max_entries=MAX_IMAGES)
+
+    train_images, train_mean_age, train_ages, train_get_age = read_data("/work/forkert_lab/erik/T1_warped/train", postfix=".nii.gz", max_entries=MAX_IMAGES*.8)
+    val_images, val_mean_age, val_ages, val_get_age = read_data("/work/forkert_lab/erik/T1_warped/val", postfix=".nii.gz", max_entries=MAX_IMAGES*.1)
+    test_images, test_mean_age, test_ages, test_get_age = read_data("/work/forkert_lab/erik/T1_warped/test", postfix=".nii.gz", max_entries=MAX_IMAGES*.1)
 
 
     # Add transforms to the dataset
@@ -48,46 +52,46 @@ def main(rank, world_size):
     transforms = Compose([customTransforms.Crop3D((150,150,150)),EnsureChannelFirst(), NormalizeIntensity()])
 
     # Define image dataset, data loader
-    ds = ImageDataset(image_files=images, labels=ages, dtype=np.float32, transform=transforms,reader="NibabelReader")
-    # plt.imshow(ds[0][0][0][80])
-    # plt.show()
+    train_ds = ImageDataset(image_files=train_images, labels=train_ages, dtype=np.float32, transform=transforms,reader="NibabelReader")
+    val_ds = ImageDataset(image_files=val_images, labels=val_ages, dtype=np.float32, transform=transforms,reader="NibabelReader")
+    test_ds = ImageDataset(image_files=test_images, labels=test_ages, dtype=np.float32, transform=transforms,reader="NibabelReader")
 
     # Split the data into training and testing sets
-    train_ds, val_ds, test_ds = torch.utils.data.random_split(ds, [.8, .1, .1])
+    # train_ds, val_ds, test_ds = torch.utils.data.random_split(ds, [.8, .1, .1])
 
-    train_list_eid = [train_ds.dataset.image_files[x] for x in train_ds.indices]
-    val_list_eid = [val_ds.dataset.image_files[x] for x in val_ds.indices]
-    test_list_eid = [test_ds.dataset.image_files[x] for x in test_ds.indices]
+    # train_list_eid = [train_ds.dataset.image_files[x] for x in train_ds.indices]
+    # val_list_eid = [val_ds.dataset.image_files[x] for x in val_ds.indices]
+    # test_list_eid = [test_ds.dataset.image_files[x] for x in test_ds.indices]
 
     # print(train_list_eid)
     # print(val_list_eid)
     # print(test_list_eid)
 
-    with open("/home/finn.vamosi/3Brain/split/train.txt", "w") as file:
-        for name in train_list_eid:
-            file.write("%s\n" % name)
-
-    with open("/home/finn.vamosi/3Brain/split/val.txt", "w") as file:
-        for name in val_list_eid:
-            file.write("%s\n" % name)
-
-    with open("/home/finn.vamosi/3Brain/split/test.txt", "w") as file:
-        for name in test_list_eid:
-            file.write("%s\n" % name)
+    # with open("/home/finn.vamosi/3Brain/split/train.txt", "w") as file:
+    #     for name in train_list_eid:
+    #         file.write("%s\n" % name)
+    #
+    # with open("/home/finn.vamosi/3Brain/split/val.txt", "w") as file:
+    #     for name in val_list_eid:
+    #         file.write("%s\n" % name)
+    #
+    # with open("/home/finn.vamosi/3Brain/split/test.txt", "w") as file:
+    #     for name in test_list_eid:
+    #         file.write("%s\n" % name)
 
     train_loader = DataLoader(train_ds, shuffle=False, batch_size=BATCH_SIZE, num_workers=N_WORKERS, pin_memory=torch.cuda.is_available(), sampler=DistributedSampler(train_ds))
     val_loader = DataLoader(val_ds, shuffle=False, batch_size=BATCH_SIZE, num_workers=N_WORKERS, pin_memory=torch.cuda.is_available(), sampler=DistributedSampler(val_ds))
     test_loader = DataLoader(test_ds, shuffle=False, batch_size=BATCH_SIZE, num_workers=N_WORKERS, pin_memory=torch.cuda.is_available(), sampler=DistributedSampler(test_ds))
     
-    if DEBUG:
-        print_title("Image Properties")
-        print(f"Max Tensor Value: {torch.max(ds[0][0])} Min Tensor Value: {torch.min(ds[0][0])}")
-        print(f"Shape of the image {ds[0][0].shape}")
-        print_title("Loading the data")
-        print(f"length of ds: {len(ds)} ")
-        print(f"Mean Age: {mean_age}")
-        print_title("Data Splitting")
-        print(f"Train: {len(train_ds)} Val: {len(val_ds)} Test: {len(test_ds)}")
+    # if DEBUG:
+    #     print_title("Image Properties")
+    #     print(f"Max Tensor Value: {torch.max(ds[0][0])} Min Tensor Value: {torch.min(ds[0][0])}")
+    #     print(f"Shape of the image {ds[0][0].shape}")
+    #     print_title("Loading the data")
+    #     print(f"length of ds: {len(ds)} ")
+    #     print(f"Mean Age: {mean_age}")
+    #     print_title("Data Splitting")
+    #     print(f"Train: {len(train_ds)} Val: {len(val_ds)} Test: {len(test_ds)}")
 
     # # Check if CUDA is available
     # torch.cuda._lazy_init()
@@ -164,14 +168,14 @@ def main(rank, world_size):
                 # Calculate the losses
                 MSE_loss = MSELoss_fn(pred, test_Y)
                 MAE_loss = MAELoss_fn(pred, test_Y)
-                MAE_with_mean_loss = MAE_with_mean_fn(mean_age, test_Y)
+                MAE_with_mean_loss = MAE_with_mean_fn(val_mean_age, test_Y)
 
                 MSE_losses.append(MSE_loss.item())
                 MAE_losses.append(MAE_loss.item())
                 MAE_with_mean_losses.append(MAE_with_mean_loss.item())
 
                 # Display Loss
-                pbar2.set_description(f"Epoch {epoch+1:<2} MSE Loss: {MSE_loss.item():<.4f} MAE Loss: {MAE_loss.item():<.4f} Last Predicted Age: {get_age(pred[-1].item()):<.4f} Last Actual Age: {get_age(test_Y[-1].item()):<.4f}")
+                pbar2.set_description(f"Epoch {epoch+1:<2} MSE Loss: {MSE_loss.item():<.4f} MAE Loss: {MAE_loss.item():<.4f} Last Predicted Age: {val_get_age(pred[-1].item()):<.4f} Last Actual Age: {val_get_age(test_Y[-1].item()):<.4f}")
         
         # Epoch over
         schdlr.step()
@@ -221,14 +225,14 @@ def main(rank, world_size):
             # Calculate the losses
             MSE_loss = MSELoss_fn(pred, test_Y)
             MAE_loss = MAELoss_fn(pred, test_Y)
-            MAE_with_mean_loss = MAE_with_mean_fn(mean_age, test_Y)
+            MAE_with_mean_loss = MAE_with_mean_fn(test_mean_age, test_Y)
 
             MSE_losses.append(MSE_loss.item())
             MAE_losses.append(MAE_loss.item())
             MAE_with_mean_losses.append(MAE_with_mean_loss.item())
 
             for i, ith_pred in enumerate(pred):
-                df.loc[len(df)] = {"Age":test_Y[i].item(), "Prediction":ith_pred.item(), "ABSError": abs(test_Y[i].item() - ith_pred.item()), "ABSMEANError": abs(test_Y[i].item() - mean_age)}
+                df.loc[len(df)] = {"Age":test_Y[i].item(), "Prediction":ith_pred.item(), "ABSError": abs(test_Y[i].item() - ith_pred.item()), "ABSMEANError": abs(test_Y[i].item() - test_mean_age)}
     
     # End of testing
     print_title("End of Testing")
